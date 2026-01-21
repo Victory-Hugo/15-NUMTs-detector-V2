8ae4428b5e451b7d62ded4d87c3e7ec2935fa34c
Victory-Hugo <giantlinlinlin@gmail.com>
2026-01-21 22:16:41 +0800

    2026年1月21日

--- 3-可视化NUMTs分布/2-Ideogram/script/1-run.sh
+++ 3-可视化NUMTs分布/2-Ideogram/script/1-run.sh
@@ -6,29 +6,31 @@ OUTPUT_NUMT_HEATMAP="/mnt/f/Onedrive/文档（科研）/脚本/Download/15-NUMTs
 OUTPUT_PNG="/mnt/f/Onedrive/文档（科研）/脚本/Download/15-NUMTs-detector-V2/3-可视化NUMTs分布/2-Ideogram/output/NUMTs_gene_density.png"
 OUTPUT_NUMT_HEATMAP_PNG="/mnt/f/Onedrive/文档（科研）/脚本/Download/15-NUMTs-detector-V2/3-可视化NUMTs分布/2-Ideogram/output/NUMTs_heatmap.png"
 PNG_DPI=600
+PNG_WIDTH=4000
+PNG_HEIGHT=4000
 KARYOTYPE_TXT="/mnt/f/Onedrive/文档（科研）/脚本/Download/15-NUMTs-detector-V2/3-可视化NUMTs分布/2-Ideogram/conf/human_karyotype.txt"
 R_SRC="/mnt/f/Onedrive/文档（科研）/脚本/Download/15-NUMTs-detector-V2/3-可视化NUMTs分布/2-Ideogram/script/2-NUMTs分布.R"
 #? 使用R包Ideogram绘制
 # "quantile" 或改成 "log" ,"linear"
 # bin_size: 聚合窗口大小（bp），设置越大，运算速度越快
-# Rscript $R_SRC \
-#     --input "$INPUT_TXT" \
-#     --karyotype "$KARYOTYPE_TXT" \
-#     --gene_color "#273e55,#6aa18a,#b5c3b1,#d7bbaf,#b26966,#782b55,#591c4b" \
-#     --numt_color "#1f3a5f,#3f6fa6,#6fa6c9,#d9b1a3,#c46a6a,#8b2e3c" \
-#     --numt_color_mode "quantile" \
-#     --numt_shape "triangle" \
-#     --bin_size 10000 \
-#     --output "$OUTPUT_SVG" \
-#     --output_numt_heatmap "$OUTPUT_NUMT_HEATMAP"
+Rscript $R_SRC \
+    --input "$INPUT_TXT" \
+    --karyotype "$KARYOTYPE_TXT" \
+    --gene_color "#273e55,#6aa18a,#b5c3b1,#d7bbaf,#b26966,#782b55,#591c4b" \
+    --numt_color "#1f3a5f,#3f6fa6,#6fa6c9,#d9b1a3,#c46a6a,#8b2e3c" \
+    --numt_color_mode "quantile" \
+    --numt_shape "triangle" \
+    --bin_size 25000 \
+    --output "$OUTPUT_SVG" \
+    --output_numt_heatmap "$OUTPUT_NUMT_HEATMAP"
 
 # Convert SVG to PNG (prefer rsvg-convert, fallback to inkscape)
 if command -v rsvg-convert >/dev/null 2>&1; then
-    rsvg-convert -a --dpi-x "$PNG_DPI" --dpi-y "$PNG_DPI" -o "$OUTPUT_PNG" "$OUTPUT_SVG"
-    rsvg-convert -a --dpi-x "$PNG_DPI" --dpi-y "$PNG_DPI" -o "$OUTPUT_NUMT_HEATMAP_PNG" "$OUTPUT_NUMT_HEATMAP"
+    rsvg-convert -a --dpi-x "$PNG_DPI" --dpi-y "$PNG_DPI" --width "$PNG_WIDTH" --height "$PNG_HEIGHT" -o "$OUTPUT_PNG" "$OUTPUT_SVG"
+    rsvg-convert -a --dpi-x "$PNG_DPI" --dpi-y "$PNG_DPI" --width "$PNG_WIDTH" --height "$PNG_HEIGHT" -o "$OUTPUT_NUMT_HEATMAP_PNG" "$OUTPUT_NUMT_HEATMAP"
 elif command -v inkscape >/dev/null 2>&1; then
-    inkscape "$OUTPUT_SVG" --export-type=png --export-filename="$OUTPUT_PNG" --export-dpi="$PNG_DPI" >/dev/null 2>&1
-    inkscape "$OUTPUT_NUMT_HEATMAP" --export-type=png --export-filename="$OUTPUT_NUMT_HEATMAP_PNG" --export-dpi="$PNG_DPI" >/dev/null 2>&1
+    inkscape "$OUTPUT_SVG" --export-type=png --export-filename="$OUTPUT_PNG" --export-dpi="$PNG_DPI" --export-width="$PNG_WIDTH" --export-height="$PNG_HEIGHT" >/dev/null 2>&1
+    inkscape "$OUTPUT_NUMT_HEATMAP" --export-type=png --export-filename="$OUTPUT_NUMT_HEATMAP_PNG" --export-dpi="$PNG_DPI" --export-width="$PNG_WIDTH" --export-height="$PNG_HEIGHT" >/dev/null 2>&1
 else
     echo "SVG->PNG failed: install rsvg-convert or inkscape." >&2
 fi
